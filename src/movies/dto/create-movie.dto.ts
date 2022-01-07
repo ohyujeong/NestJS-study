8d417eee09d288f87e9d88cfc7a1c06c561f5d3a
ohyujeong <ohlife4@gmail.com>
2022-01-07 19:36:32 +0900

    2.5 UpdateDTO

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,12 +1,13 @@
-import { IsNumber, IsString } from "class-validator";
+import { IsNumber, IsObject, IsOptional, IsString } from "class-validator";
 
 export class CreateMovieDto {
     @IsString()
-    
     readonly title: string;
+
     @IsNumber()
     readonly year: number;
 
+    @IsOptional()
     @IsString({each: true})
     readonly genres: string[];
 }
