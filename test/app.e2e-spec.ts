432ec675ede01c44b0f6e98d26c65c58cbd881a4
ohyujeong <ohlife4@gmail.com>
2022-01-09 12:11:41 +0900

    4.2 Testing patch and delete

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -36,7 +36,7 @@ describe('AppController (e2e)', () => {
         .expect(200)
         .expect([]);
     });
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -46,13 +46,24 @@ describe('AppController (e2e)', () => {
         })
         .expect(201);
     });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing'
+        })
+        .expect(400);
+    });
     it('DELETE', () => {
       return request(app.getHttpServer())
         .delete('/movies')
         .expect(404);
     });
   });
-  
+
   describe('/movies/:id', () => {
     it('GET 200', () => {
       return request(app.getHttpServer())
@@ -64,7 +75,16 @@ describe('AppController (e2e)', () => {
         .get('/movies/999')
         .expect(404);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer())
+        .delete('/movies/1')
+        .expect(200)
+    });
   });
 });
